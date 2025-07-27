f1aa699f7b03c5c9021b0c1eb510eb5e04537bb8
wayneheim <wayneheim@aol.com>
2025-07-27 14:05:43 -0400

    Fix Page Width

--- src/components/RebuiltScrollGrid.jsx
+++ src/components/RebuiltScrollGrid.jsx
@@ -184,12 +184,12 @@ useEffect(() => {
     willChange: "transform",
   }}
   onMouseEnter={(e) => {
-    e.currentTarget.style.transition = "transform 3.29s ease-out";
+    e.currentTarget.style.transition = "transform 5.29s ease-out";
     e.currentTarget.style.transform =
-      "scale(1.15) translate(-4%, -4%) rotate(-1.25deg)";
+      "scale(1.1) translate(-4%, -4%) rotate(-1.2deg)";
   }}
   onMouseLeave={(e) => {
-    e.currentTarget.style.transition = "transform 0.1s ease-in";
+    e.currentTarget.style.transition = "transform 0.051s ease-in";
     e.currentTarget.style.transform = "none";
   }}
   onError={(e) => {
