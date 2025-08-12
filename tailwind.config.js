9b874ea62f9e8765edab2b1b861d1a02c747dcef
wayneheim <wayneheim@aol.com>
2025-08-12 13:21:39 -0400

    Use local Tailwind v4 utilities; remove Tailwind v2 CDN

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    './src/**/*.{astro,js,ts,jsx,tsx}',
+    './components/**/*.{astro,js,ts,jsx,tsx}',
+    './layouts/**/*.{astro,js,ts}',
+    './pages/**/*.{astro,js,ts}',
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
