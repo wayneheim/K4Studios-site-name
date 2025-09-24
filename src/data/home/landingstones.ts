aeae57ca000eb24ebcc77ed0bac10871d7e992c3
wayneheim <wayneheim@aol.com>
2025-09-23 21:54:43 -0400

    animated home

--- src/data/home/landingstones.ts
+++ src/data/home/landingstones.ts
@@ -8,17 +8,17 @@ export const landingWestern = {
     {
       title: 'Painterly Style Photography',
       href: '/Galleries/Painterly-Fine-Art-Photography',
-      thumb: 'https://photos.smugmug.com/Galleries/Painterly-Fine-Art-Photography/Landscapes/By-Location/Midwest/Gallery/i-mCJv3RM/0/MdwvnZ7LTST6BHvQG527fBMhQ4pmKsgpxCjZ9DfKc/S/_WHZ7317-Edit-Edit-Edit-S.jpg',
+      thumb: '/images/tombstones/Painterly-ts.webp',
     },
     {
       title: 'Traditional Style Photography',
       href: '/Galleries/Fine-Art-Photography',
-      thumb: 'https://photos.smugmug.com/Galleries/Fine-Art-Photography/Architecture/Gallery/i-3R8rc7C/5/LMfd4hKLM8FchM8pbdHwfRmRCFb84H3GjzWnwnLhk/S/_IMG2241-S.jpg',
+      thumb: '/images/tombstones/traditional-ts.webp',
     },
      {
       title: 'Engrained Series',
       href: '/Other/K4-Select-Series/Engrained',
-      thumb: '/images/tombstones/engrained-ts.jpg',
+      thumb: '/images/tombstones/engrained-ts.webp',
     },
   ]
 };
